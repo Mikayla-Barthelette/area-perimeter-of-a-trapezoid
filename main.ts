a178cc4be9317d94f36e58da6df1720bf588a696
Mikayla-Barthelette <71400623+Mikayla-Barthelette@users.noreply.github.com>
2020-10-06 14:42:01 -0400

    how to fix area

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@
 // Created on: Oct 2020
 // 
 // This program allows us to calculate the perimeter and area of a trapezoid
+let base_1_and_2 = 0
 game.splash("Let's calculate the area and perimeter of a trapezoid.")
 let Base_1 = game.askForNumber("Enter the measurement of base 1 (cm):")
 let Base_2 = game.askForNumber("Enter the measurement of base 2 (cm):")
@@ -9,6 +10,8 @@ let Height = game.askForNumber("Enter the measurement of height (cm):")
 let Diagonal = game.askForNumber("Enter the measurement of the diagonal line (cm):")
 let Perimeter = Base_1 + (Base_2 + (Height + Diagonal))
 game.splash("The perimeter of a trapezoid dimensions:" + Base_1 + "cm by " + Base_2 + "cm by " + Height + "cm by " + Diagonal + "cm is " + convertToText(Perimeter) + "cm")
-let Area = Base_1 + Base_2 / (2 * Height)
-game.splash("The area of a trapezoid dimensions:" + Base_1 + "cm by " + Base_2 + "cm divided by " + "2" + "cm multiplied by " + Height + "cm is " + convertToText(Area) + "cm2")
+let Area = Base_1 + Base_2
+let real_area = Base_1 + Base_2 == base_1_and_2
+let total_area = base_1_and_2 / (2 * Height)
+game.splash("The area of a trapezoid dimensions:" + Base_1 + "cm by " + Base_2 + "cm divided by " + "2" + "cm multiplied by " + Height + "cm is " + convertToText(total_area) + "cm2")
 game.splash("Done")
