e171f56b83ce0e01b985d3d4cfcdaf04f912dafc
Mikayla-Barthelette <71400623+Mikayla-Barthelette@users.noreply.github.com>
2020-10-08 13:52:02 -0400

    real final commit

--- main.ts
+++ main.ts
@@ -2,16 +2,17 @@
 // Created on: Oct 2020
 // 
 // This program allows us to calculate the perimeter and area of a trapezoid
-let base_1_and_2 = 0
 game.splash("Let's calculate the area and perimeter of a trapezoid.")
 let Base_1 = game.askForNumber("Enter the measurement of base 1 (cm):")
 let Base_2 = game.askForNumber("Enter the measurement of base 2 (cm):")
 let Height = game.askForNumber("Enter the measurement of height (cm):")
 let Diagonal = game.askForNumber("Enter the measurement of the diagonal line (cm):")
 let Perimeter = Base_1 + (Base_2 + (Height + Diagonal))
-game.splash("The perimeter of a trapezoid dimensions:" + Base_1 + "cm by " + Base_2 + "cm by " + Height + "cm by " + Diagonal + "cm is " + convertToText(Perimeter) + "cm")
-let Area = Base_1 + Base_2
-let real_area = Base_1 + Base_2 == base_1_and_2
-let total_area = base_1_and_2 / (2 * Height)
-game.splash("The area of a trapezoid dimensions:" + Base_1 + "cm by " + Base_2 + "cm divided by " + "2" + "cm multiplied by " + Height + "cm is " + convertToText(total_area) + "cm2")
+game.splash("The perimeter of a trapezoid dimensions:" + Base_1 + " cm by " + Base_2 + " cm by " + Height + " cm by " + Diagonal + " cm is " + convertToText(Perimeter) + "cm")
+let first_area = Base_1 + Base_2
+game.splash("The first area of a trapezoid dimensions:" + Base_1 + "cm plus " + Base_2 + "cm is " + first_area + "cm")
+let second_area = first_area / 2
+game.splash("The area of a trapezoid is: " + first_area + " cm divided by " + " 2 " + " cm is " + second_area + "cm")
+let Area = second_area * Height
+game.splash("The area of the trapezoid is: " + second_area + " cm by " + Height + " cm is " + Area + " cm ")
 game.splash("Done")
